412088ce71a1f7280b5ece660144f1aed6d81f87
miketolcer <miketolcer@yahoo.com>
2021-12-21 12:29:23 -0500

    final

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+//1) Sum Zero
 let numArry = [0, 1, 3, 4, 5,]
 sum = 0
 
@@ -12,15 +13,23 @@ let result = numArry.filter(function(item) {
 })
 console.log(result) // O(N)
 
+//2) Unique Characters
+const charString =("Thequickbrownfoxjumpedoverthelazydogs");
 
-function uniqueCharacters(str) {
-    for(let i = 0; i < str.length; i++)
-    for(let j = i + 1; j < str.length; j++)
-        if(str[i] === str[j])
-        return false;   //O(n^2) 
-        
-} 
-console.log(uniqueCharacters)
+function unique(charString){
+    for (let i=0; i < charString.length; i++) {
+        for (let j = 0; j < charString.length; j++) {
+            if (charString[i] === charString[j])
+            return false;
+
+            return true;
+        }
+    }
+}
+//O(N^2)
+unique(charString)
+
+//3) Pangram Sentence
 
 let abc = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 let dog = ['The quick brown fox jumps over the lazy dog!']
@@ -36,8 +45,28 @@ function letters(abc) {
 console.log(letters)  
 
 
-
+//4) Longest Word
 function findLongestWord(str) {
-    let strSplit = str.split('')
+    let strSplit = str.split('');
+
+    let findLongestWord = "";
+    for(let index = 0; index < splStrArray.length; index++) {
+        if(splStrArray[index].length > findLongestWord.length) {
+            longestWord = splStrArray[index];
+        }
+    }
+  return longestWord
 
 }
+//O(N^2)
+const perf = require('execution-time')();
+perf.start();
+findLongestWord("The quick brown fox jumped over the lazy dogs");
+let results = perf.stop();
+console.log('Results for the longest word');
+console.log(results);
+
+// time: 0.3358,
+// words: '242 μs',
+// preciseWords: '255.9 μs',
+// verboseWords: '225 microseconds 900 nanoseconds'
